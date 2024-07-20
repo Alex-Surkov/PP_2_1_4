3b0928393a331bb924a1e34f528fcc81d8145f10
Alex-Surkov <127119419+Alex-Surkov@users.noreply.github.com>
2024-07-20 18:21:39 +0300

    test passed. Ready for check.

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,6 +1,18 @@
 package app.model;
 
+import org.springframework.stereotype.Component;
+
+@Component("timer")
 public class Timer {
+    public Timer() {
+    }
+
+    @Override
+    public String toString() {
+        return "Timer{" +
+                "nanoTime=" + nanoTime +
+                '}';
+    }
 
     private Long nanoTime = System.nanoTime();
 
